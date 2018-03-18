dffa9a0e33c950764633c56913a1902353a8804e
Amber Sharpe <amberjsharpe@gmail.com>
2018-03-18 11:46:51 -0500

    initial setup, html, bootstraps stylings. Began work on Google user authentication

---
+++ js/fb-key.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Get get key from Firebase
+function getKey() {
+  return {
+    apiKey: "AIzaSyC24z4XMJSUuZFNhRpfM9w4JQdU5OQzAMg",
+    authDomain: "goodreads-bdde5.firebaseapp.com",
+    databaseURL: "https://party-crashers-2c54b.firebaseio.com"
+  };
+}
+
+module.exports = getKey;
