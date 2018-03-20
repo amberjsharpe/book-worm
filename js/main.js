c71548c725d6cae5db50cfdfd1f5569f382d6a44
Amber Sharpe <amberjsharpe@gmail.com>
2018-03-20 12:58:10 -0500

    added requires to wishlist.js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ let config = require("./fb-config"),
     key = require("./fb-key"),
     user = require("./user"),
     search = require("./search-api");
-    require("./event-listeners");
+    event = require("./event-listeners");
--- js/wishlist.js
+++ js/wishlist.js
@@ -1,1 +1,7 @@
 "use strict";
+
+let config = require("./fb-config"),
+    key = require("./fb-key"),
+    user = require("./user"),
+    search = require("./search-api");
+    event = require("./event-listeners");
