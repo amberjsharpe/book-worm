c6013688072b635dbeaf11f544cf139e18e9051f
Amber Sharpe <amberjsharpe@gmail.com>
2018-03-19 20:04:05 -0500

    writing dom functions

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -4,8 +4,6 @@ let $ = require("../lib/node_modules/jquery");
 
 
 $(".search-btn").on("click", function(event) {
-    console.log(event);
-    event.preventDefault();
-    console.log("search");
     search.searchInputValue();
+    event.preventDefault();
 });
--- js/search-api.js
+++ js/search-api.js
@@ -4,29 +4,89 @@ let $ = require("../lib/node_modules/jquery");
 // Pull API
 function getBooks(searchBooks) {
     return $.ajax({
-        url: `https://www.goodreads.com/search.xml?key=Fnqk8bj6Up42xHAAc3anFg&q='${searchBooks}'`,
-    })
+        url: `https://crossorigin.me/https://www.goodreads.com/search.xml?key=Fnqk8bj6Up42xHAAc3anFg&q='${searchBooks}'`,
+        type: "GET",
+        dataType: "xml"
+    });
 } 
 
 // Get value from Search Input
 let searchInputValue = () => {
     let value = $("#search").val();
-    console.log(value);
-    $('#display').empty();
-    console.log($('#search').val('')); 
+    printBooks(value);
+    // $('#display').empty();
+    // console.log($('#search').val('')); 
 };
 
+function xmlToJson(xml) {
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+}
+
 // Returns promise - .then() where I call it
-// let booksArray = [];
-// function printBooks(value){
-//     getBooks(value)
-//    .then((books) => {
-//     console.log(books);
-//     let booksData = XML.parse(books);
-//     console.log(booksArray);
-//     booksArray.push(booksData);
-//     // print function
-//    });
+let booksArray = [];
+let booksInfoArray = [];
+function parseBooks(value){
+    getBooks(value).then((books) => {
+        console.log(books);
+        let jsonText = xmlToJson(books);
+        let booksData = jsonText.GoodreadsResponse.search.results.work;
+        booksArray.push(booksData);
+        console.log(booksArray);
+            for (var i = 0; i < booksArray.length; i++) {
+                booksInfoArray.push(booksArray[i].best_book);
+                console.log(booksInfoArray);
+            }
+        // print function
+        // printSearchResultsToDOM();
+    });
+}
+
+// let printSearchResultsToDOM = () => {
+//     for (var i = 0; i < booksArray.length; i++) {
+//         var bookDiv = 
+//             `<div class="bookDisplay">
+//             <h2 class="listing__title">:${booksArray[i]}</h2> 
+//             </div>
+//         `;
+//     document.querySelector("display").innerHTML += bookDiv;
+//     }
+
+
 // }
 
-module.exports = {getBooks, searchInputValue};
+// https://stackoverflow.com/questions/4499652/accessing-a-json-variable-pre-fixed-with-a-hash
+
+module.exports = {getBooks, searchInputValue, xmlToJson, parseBooks};
