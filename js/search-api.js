8912c1c44cce687a8691a5979457d2cc9557e4bb
Amber Sharpe <amberjsharpe@gmail.com>
2018-03-21 11:39:00 -0500

    changed function name

--- js/search-api.js
+++ js/search-api.js
@@ -1,92 +1,79 @@
-"use strict";
-let $ = require("../lib/node_modules/jquery");
-let user = require("./user");
-let wishlist = require("./wishlist");
-let readBooks = require("./mark-read");
-let getUser = user.getUser;
-
+'use strict';
+let $ = require('../lib/node_modules/jquery');
+let wishlist = require('./wishlist');
+let readBooks = require('./mark-read');
 // Pull API
 function getBooks(searchBooks) {
     return $.ajax({
         url: `https://crossorigin.me/https://www.goodreads.com/search.xml?key=Fnqk8bj6Up42xHAAc3anFg&q='${searchBooks}'`,
-        type: "GET",
-        dataType: "xml"
+        type: 'GET',
+        dataType: 'xml'
     });
-} 
-
+}
 // Get value from Search Input
 let searchInputValue = () => {
     $('#display').empty();
-    let value = $("#search").val();
+    let value = $('#search').val();
     parseAndPrintBooks(value);
     $('#search').val('');
 };
-
 function xmlToJson(xml) {
-	// Create the return object
-	var obj = {};
-
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
-
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
+    // Create the return object
+    var obj = {};
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+        obj['@attributes'] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
+    }
+    // do children
+    if (xml.hasChildNodes()) {
+        for(var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof(obj[nodeName]) === 'undefined') {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof(obj[nodeName].push) === 'undefined') {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
 }
-
 // Search and Home Page
 let booksArray = [];
-
 function parseAndPrintBooks(value){
     getBooks(value).then((books) => {
-        console.log(books);
         let jsonText = xmlToJson(books);
         let booksData = jsonText.GoodreadsResponse.search.results.work;
         booksData.forEach(function(item) {
             var book = item.best_book;
             let bookObject = {
-                title: book.title["#text"],
-                author: book.author.name["#text"],
-                image_url: book.image_url["#text"],
-                id: book.id["#text"]
+                title: book.title['#text'],
+                author: book.author.name['#text'],
+                image_url: book.image_url['#text'],
+                id: book.id['#text']
             };
             booksArray.push(bookObject);
-        });         
+        });
         printSearchResultsToDOM(booksArray);
-        console.log(booksArray);
     });
 }
-
 let printSearchResultsToDOM = (booksArray) => {
-    $('#display').append(`<div><h2>Search Results</h2></div>`);
-    
+    $('#display').append('<div><h2>Search Results</h2></div>');
     for (var i = 0; i < booksArray.length; i++) {
-        var bookDiv = 
+        var bookDiv =
             `<div class="bookDisplay">
             <h3 class="title">${booksArray[i].title}</h3>
             <h4 class="author">Author: ${booksArray[i].author}</h4>
@@ -95,23 +82,19 @@ let printSearchResultsToDOM = (booksArray) => {
             <button id="${booksArray[i].id}-read" class="markread-btn btn search-btn btn-outline-success my-2 my-sm-0">Mark as Read</button>
             </div>
         `;
-        document.querySelector("#display").innerHTML += bookDiv;
+        document.querySelector('#display').innerHTML += bookDiv;
     }
 };
-
 // Wishlist
-
 let printWishlistToDOM = (wishData) => {
     $('#display').empty();
-    $('#display').append(`<div><h2>My Wishlist</h2></div>`);
-
+    $('#display').append('<div><h2>My Wishlist</h2></div>');
     let wishlistArray = [];
     for (let item in wishData) {
         wishlistArray.push(wishData[item]);
     }
-
     for (var i = 0; i < wishlistArray.length; i++) {
-        var bookDiv = 
+        var bookDiv =
             `<div class="bookDisplay">
             <h3 class="title">${wishlistArray[i].title}</h3>
             <h4 class="author">Author: ${wishlistArray[i].author}</h4>
@@ -120,72 +103,58 @@ let printWishlistToDOM = (wishData) => {
             <button class="delete-btn btn search-btn btn-outline-success my-2 my-sm-0">Delete</button>
             </div>
         `;
-        document.querySelector("#display").innerHTML += bookDiv;
+        document.querySelector('#display').innerHTML += bookDiv;
     }
 };
-
 let checkWishListButton = (event) => {
     let matchedBook = booksArray.filter(i => i.id === event.target.id)[0];
     wishlist.addToWishlist(matchedBook).then(wishlistData => {
         console.log(wishlistData);
-    });   
+    });
 };
-
 let getWishListData = () => {
     wishlist.getWishList().then(wishlistData => {
         printWishlistToDOM(wishlistData);
     });
 };
-
 // Mark as Read
-
 let printReadBooksToDOM = (readData) => {
     $('#display').empty();
-    $('#display').append(`<div><h2>Books I've Read</h2></div>`);
-
+    $('#display').append('<div><h2>Books I\'ve Read</h2></div>');
     let booksReadArray = [];
     for (let item in readData) {
         booksReadArray.push(readData[item]);
-        console.log("readData", readData[item]);
-        console.log("booksReadArray", booksReadArray);
     }
-
     for (var i = 0; i < booksReadArray.length; i++) {
-        var bookDiv = 
+        var bookDiv =
             `<div class="bookDisplay">
             <h3 class="title">${booksReadArray[i].title}</h3>
             <h4 class="author">Author: ${booksReadArray[i].author}</h4>
             <img class="book-img" src="${booksReadArray[i].image_url}">
-            <button id="${booksReadArray[i].id}-read" class="markread-btn btn search-btn btn-outline-success my-2 my-sm-0">Mark as Read</button>
+            <button class="markread-btn btn search-btn btn-outline-success my-2 my-sm-0">Mark as Read</button>
             <button class="delete-btn btn search-btn btn-outline-success my-2 my-sm-0">Delete</button>
             </div>
         `;
-        document.querySelector("#display").innerHTML += bookDiv;
-        console.log(booksReadArray);
+        document.querySelector('#display').innerHTML += bookDiv;
     }
 };
-
 let checkBooksReadButton = (event) => {
-    console.log(event);
     let matchedBook = booksArray.filter(i => `${i.id}-read` === `${event.target.id}`)[0];
     readBooks.addToMarkRead(matchedBook).then(readData => {
-        console.log(matchedBook);
         console.log(readData);
-    });   
+    });
 };
-
 let getReadBooksData = () => {
     readBooks.getReadBooks().then(readData => {
         printReadBooksToDOM(readData);
     });
 };
-
 module.exports = {
-    getBooks, 
-    searchInputValue, 
-    xmlToJson, 
-    parseAndPrintBooks, 
-    printSearchResultsToDOM, 
+    getBooks,
+    searchInputValue,
+    xmlToJson,
+    parseAndPrintBooks,
+    printSearchResultsToDOM,
     checkWishListButton,
     getWishListData,
     checkBooksReadButton,
